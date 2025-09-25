9b1689cc9cba130db81da239d79ea8a382400137
theo-thanhlam <theo.thanhlam@gmail.com>
2025-09-25 17:44:45 -0400

    MODIFY: remove headline

--- libs/rss/cbcParser.py
+++ libs/rss/cbcParser.py
@@ -1,22 +1,4 @@
 from .rssParser import RSSParser
 
 class CBCParser(RSSParser):
-    def getItems(self):
-        items = []
-        for entry in self.data.entries: 
-            id = entry.id
-            title = entry.title
-            link = entry.link
-           
-            published = entry.published
-            
-            item = {
-                "id":id,
-                "title":title,
-                "url":link,
-                
-                "published":published,
-                
-            }
-            items.append(item)   
-        return items
+    pass
--- libs/rss/rssParser.py
+++ libs/rss/rssParser.py
@@ -1,16 +1,35 @@
 import feedparser
 from configs import headers
-from abc import ABC, abstractmethod
 from typing import List
 from .rss_item import RSSItem
 import requests
 
-class RSSParser(ABC):
+class RSSParser:
     def __init__(self, rssUrl):
 
         self.data = feedparser.parse(rssUrl, request_headers=headers)
+        self.items = []
+        self.parseItems()
         
-    @abstractmethod
+        
+    
+    def parseItems(self):
+        for entry in self.data.entries: 
+            id = entry.id
+            title = entry.title
+            link = entry.link
+            published = entry.published
+            
+            item = {
+                "id":id,
+                "title":title,
+                "url":link,          
+                "published":published,
+                
+            }
+            self.items.append(item) 
+        
+    
     def getItems(self) -> List[RSSItem]:
         """Return a list of items from rss feed """
-        pass
+        return self.items
