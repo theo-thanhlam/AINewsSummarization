cecd7d6f43964b0140b66109d1eead62f4537bcf
theo-thanhlam <phatlam.media.vis@gmail.com>
2025-10-01 17:14:17 -0400

    ADD: error handling

--- tasks/topStories.py
+++ tasks/topStories.py
@@ -40,49 +40,52 @@ def fetchCBCTopStories():
     db = CRUD()
     summerizeAgent = SummarizeAgent()
     for item in items:
-        data = parseItem(item)
+        try:
+            data = parseItem(item)
+            
+            #Handle author operations
+            authorDoc = data['author']
+            if authorDoc:
+                existing_author = db.getOneBy(Author, name=authorDoc['name'])
+                if not existing_author:
+                    new_author = Author(name=authorDoc['name'], url = authorDoc['url'])
+                    db.create(new_author)
+                    
         
-        #Handle author operations
-        authorDoc = data['author']
-        if authorDoc:
-            existing_author = db.getOneBy(Author, name=authorDoc['name'])
-            if not existing_author:
-                new_author = Author(name=authorDoc['name'], url = authorDoc['url'])
-                db.create(new_author)
-                
-       
-        # Handle aricle operations
-        articleDoc = data
-        article_author=articleDoc['author']
-        
-        existing_article = db.getOneBy(Article, article_id=articleDoc['id'])
-        if existing_article:
-            continue
-        
-        
-        author_id:int = None
-        if article_author:
-            author_id = db.getOneBy(Author, name=article_author['name']).id
-        
-        
-        new_article = Article(article_id=articleDoc['id'], 
-                              title=articleDoc['title'], 
-                              story=articleDoc['story'],
-                              url=articleDoc['url'], 
-                              published=articleDoc['published'],
-                              author_id=author_id,
-                              broadcaster_id = 1
-                              )
-        db.create(new_article)
-        
-        #Handle AI Summaries
-        ai_response = summerize(data,summerizeAgent=summerizeAgent)
-        new_summary = Summary(article_id = new_article.id, summary = ai_response.summary)
-        db.create(new_summary)
-        
-        for takeaway in ai_response.key_takeaways:
-            new_takeaway = Takeaway(summary_id = new_summary.id, takeaway=takeaway)
-            db.create(new_takeaway)
+            # Handle aricle operations
+            articleDoc = data
+            article_author=articleDoc['author']
+            
+            existing_article = db.getOneBy(Article, article_id=articleDoc['id'])
+            if existing_article:
+                continue
+            
+            
+            author_id:int = None
+            if article_author:
+                author_id = db.getOneBy(Author, name=article_author['name']).id
+            
+            
+            new_article = Article(article_id=articleDoc['id'], 
+                                title=articleDoc['title'], 
+                                story=articleDoc['story'],
+                                url=articleDoc['url'], 
+                                published=articleDoc['published'],
+                                author_id=author_id,
+                                broadcaster_id = 1
+                                )
+            db.create(new_article)
+            
+            #Handle AI Summaries
+            ai_response = summerize(data,summerizeAgent=summerizeAgent)
+            new_summary = Summary(article_id = new_article.id, summary = ai_response.summary)
+            db.create(new_summary)
+            
+            for takeaway in ai_response.key_takeaways:
+                new_takeaway = Takeaway(summary_id = new_summary.id, takeaway=takeaway)
+                db.create(new_takeaway)
+        except :
+            print(f"Error fetching {item['url']}")
 
 
 if __name__ == "__main__":
