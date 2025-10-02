b0247718365f40296c6941c3007debddf7e4a904
theo-thanhlam <phatlam.media.vis@gmail.com>
2025-10-01 22:20:05 -0400

    ADD: timezone converter

--- libs/rss/rssParser.py
+++ libs/rss/rssParser.py
@@ -4,7 +4,7 @@ from typing import List
 from .rss_item import RSSItem
 import requests
 from datetime import datetime,timedelta
-from dateutil import parser
+from dateutil import parser,tz
 class RSSParser:
     def __init__(self, rssUrl):
 
@@ -15,8 +15,13 @@ class RSSParser:
         
     
     def parseItems(self):
+        
+        tzinfos = {
+            "EDT": tz.tzoffset("EDT", -4 * 3600),  # UTC-4
+            "EST": tz.tzoffset("EST", -5 * 3600),  # UTC-5
+        }
         for entry in self.data.entries: 
-            published = parser.parse(entry.published)  # parse the string with dateutil
+            published = parser.parse(entry.published, tzinfos=tzinfos)  # parse the string with dateutil
             today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
             tomorrow = today + timedelta(days=1)
 
