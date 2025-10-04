5a8e8607dc0aea301e2143bf9295099eda09f461
theo-thanhlam <theo.thanhlam@gmail.com>
2025-10-04 11:13:35 -0400

    ADD: templates and send message

--- libs/mailer/__init__.py
+++ libs/mailer/__init__.py
@@ -1,9 +1,1 @@
-import os
-from sendgrid import SendGridAPIClient
-from dotenv import load_dotenv
-load_dotenv()
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
-
-
-sg_client = SendGridAPIClient(SENDGRID_API_KEY)
-template_id = "d-68be7f2d51864a378525267e2d578103"
+from .smtp import Mailer
