659f1f0b185a5e7c053537e4f82f2884a498b0df
theo-thanhlam <theo.thanhlam@gmail.com>
2025-10-10 13:03:52 -0400

    MODIFY: change query based on new database design and format date

--- libs/database/queries.py
+++ libs/database/queries.py
@@ -2,27 +2,11 @@
 from . import getSession
 from models import *
 from sqlalchemy import func
-from pydantic import BaseModel
-from typing import List
+
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-
-
-class SingleNewsSummary(BaseModel):
-    article_id:int
-    article_title:str
-    article_url:str
-    # article_published_parsed:datetime
-    article_published:str
-    author_id:int
-    author_name:str
-    author_url:str
-    summary:str
-    takeaways:List[str]
-
-
-def getNewsSnapshot(limit=5):
+def getNewsSnapshot(limit=5,topic:str="Top Stories"):
     with getSession() as db:
         query = (
          db.query(
@@ -30,46 +14,42 @@ def getNewsSnapshot(limit=5):
             Article.title.label("article_title"),
             Article.url.label("article_url"),
             Article.published.label("article_published"),
-            Author.id.label("author_id"),
+            
             Author.name.label("author_name"),
             Author.url.label("author_url"),
-            Summary.summary.label("summary"),
-            func.array_agg(Takeaway.takeaway).label("takeaways")
+            Broadcaster.name.label("broadcaster_name"),
+            Topic.name.label("topic_name"),
+            Summary.summary,
+            func.array_agg(Takeaway.takeaway).label("takeaways"),
          )
-         .join(Author, Article.author_id == Author.id,isouter=True)
-         .join(Summary, Article.id == Summary.article_id)
-         .join(Takeaway, Takeaway.summary_id == Summary.id)
+         .join(Author, Article.author_id == Author.id, isouter=True)
+        .join(Broadcaster, Article.broadcaster_id == Broadcaster.id)
+        .join(Topic, Article.topic_id == Topic.id)
+        .join(Summary, Summary.article_id == Article.id)
+        .join(Takeaway, Takeaway.article_id == Article.id)
+        .filter(Topic.name==topic)
          .group_by(
             Article.id,
             Article.title,
             Article.url,
             Article.published,
-            Author.id,
             Author.name,
             Author.url,
-            Summary.summary
+            Summary.summary,
+            Broadcaster.name,
+            Topic.name,
          ).order_by(Article.published.desc()).limit(limit)
       )
         results = query.all()
         data = [
-            # SingleNewsSummary(
-            #     article_id=row.article_id,
-            #     article_title=row.article_title,
-            #     article_url=row.article_url,
-            # #    article_published_parsed=row.article_published.astimezone(ZoneInfo("America/New_York")),
-            #     article_published=row.article_published.astimezone(ZoneInfo("America/New_York")).isoformat(),
-            #     author_id = row.author_id,
-            #     author_name=row.author_name,
-            #     author_url = row.author_url,
-            #     summary=row.summary,
-            #     takeaways = row.takeaways
-            #     ).model_dump_json(indent=4,)
+           
            {
         "article_id": row.article_id,
         "article_title": row.article_title,
         "article_url": row.article_url,
+        "broadcaster_name":row.broadcaster_name,
+        "topic_name":row.topic_name,
         "article_published": row.article_published.astimezone(ZoneInfo("America/New_York")).isoformat() if row.article_published else None,
-        "author_id": row.author_id,
         "author_name": row.author_name,
         "author_url": row.author_url,
         "summary": row.summary,
@@ -78,7 +58,7 @@ def getNewsSnapshot(limit=5):
             for row in results
         ]
         return {
-            "date":datetime.now(ZoneInfo("America/New_York")).date().isoformat(),
+            "date":datetime.now(ZoneInfo("America/New_York")).strftime("%A, %b %d %I%p"),
             "data":data
         }
 
