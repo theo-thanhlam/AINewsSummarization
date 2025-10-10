5196edf2951445e88014f01c04cb268f55b190fb
theo-thanhlam <theo.thanhlam@gmail.com>
2025-10-10 13:05:04 -0400

    MODIFY: feed is no longer meta model

--- models/feed.py
+++ models/feed.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, Text, ForeignKey
 from sqlalchemy.orm import relationship
 from .base import BaseModel, BaseMetadataModel
 
-class Feed(BaseModel, BaseMetadataModel):
+class Feed(BaseModel):
     __tablename__ = "feeds"
     url = Column(Text)
     topic_id = Column(Integer, ForeignKey("topics.id"))
@@ -11,6 +11,6 @@ class Feed(BaseModel, BaseMetadataModel):
     broadcaster = relationship("Broadcaster", back_populates="feeds")
     
     def __repr__(self):
-        return f"<Feed(id={self.id}, name='{self.name}', url='{self.url}', topic_id='{self.topic_id}', broadcaster_id='{self.broadcaster_id}')>"
+        return f"<Feed(id={self.id}, url='{self.url}', topic_id='{self.topic_id}', broadcaster_id='{self.broadcaster_id}')>"
     
     
